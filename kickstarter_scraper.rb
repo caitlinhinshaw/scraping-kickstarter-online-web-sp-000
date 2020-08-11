89c6ab1b925cf4bc005e333a51810e43663a626d
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-08-11 23:09:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,9 +15,9 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-    projects
   end
   #binding.pry
+  projects
 end
 
 create_project_hash
